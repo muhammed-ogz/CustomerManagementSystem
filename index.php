aaf4a101e2a01fb0916d7344718f6bafb08b7afe
muhammed-ogz <muhammetnabioguz@gmail.com>
2024-06-09 19:56:49 +0300

    gitignore added

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Core\Request;
+use Core\Starter;
+
+require __DIR__ . "/config.php";
+require __DIR__ . "/vendor/autoload.php";
+
+$cms = new Starter();
+
+require __DIR__ . "/App/Routes/Route.php";
